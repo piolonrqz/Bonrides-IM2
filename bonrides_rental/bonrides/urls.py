f25c54a1adeedf0aa4bc1ec8fa5325acbdc2f51d
jerjenres <jerjenres.pangalay@cit.edu>
2024-11-08 00:49:45 +0800

    vehicle/car CRUD

--- bonrides_rental/bonrides/urls.py
+++ bonrides_rental/bonrides/urls.py
@@ -22,4 +22,8 @@ urlpatterns = [
     path('booking/new/', car_booking_create, name='car_booking_create'),
     path('booking/<int:pk>/edit/', car_booking_update, name='car_booking_update'),
     path('booking/<int:pk>/delete/', car_booking_delete, name='car_booking_delete'),
+    path('vehicles/add/', views.add_vehicle, name='add_vehicle'),
+    path('vehicles/manage/', views.manage_vehicles, name='manage_vehicles'),
+    path('vehicles/edit/<int:pk>/', views.edit_vehicle, name='edit_vehicle'),
+    path('vehicles/delete/<int:pk>/', views.delete_vehicle, name='delete_vehicle'),
 ]
